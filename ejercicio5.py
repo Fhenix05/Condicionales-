cf7f84d3cd8d9e4e126d07706df7b91a56d1bc41
Fhenix05 <juanluisparrasosa@gmail.com>
2024-11-04 10:00:38 -0600

    03

---
+++ ejercicio5.py
@@ -0,0 +1,12 @@
+"""
+user == user and password == password
+"""
+
+print("Nombre de usuario y contraseña")
+user = input("Escribe tu nimbre de usuario: ")
+password = input("EScribe tu contraseña: ")
+
+if user == 'juan' and password == 'j123456':
+    print("Has entrado al sistema")
+else:
+    print("Error")
