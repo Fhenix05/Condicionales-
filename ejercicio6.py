8ae606ee247a9478592176f13599c3a5880abc53
Fhenix05 <juanluisparrasosa@gmail.com>
2024-11-06 11:05:11 -0600

    04

--- ejercicio6.py
+++ ejercicio6.py
@@ -5,7 +5,7 @@ else:
     instrucciones
 """
 
-print("Progra que lea una cadena por teclado yy compruebe si es una letra mayúscula")
+print("Programa que lea una cadena por teclado y compruebe si es una letra mayúscula")
 letra = input("Escribe una letra: ")
 if len(letra) == 1:
     if letra == letra.upper():
